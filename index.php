87ab66a33b4d611abe9db3af0bebbadf6ce584c4
Julian Mahecha <jemahechaj@ut.edu.co>
2019-01-21 19:31:02 -0500

    Cracion de cabecera,  pie de pagina y barra de navegacion

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include("db.php") //Se requiere la conexion a la base de datos.?>
+<?php include("includes/header.php")?> 
+    
+
+
+
+
+<?php include("includes/footer.php") ?>
+    
+    
+    
